208ab7e938c6f9de759de1d449e836046e2dfd02
Gabriel Raposo <gabriel.robertor@gmail.com>
2023-10-14 18:02:07 -0700

    adds throttler

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,3 +1,6 @@
+import { ThrottlerGuard, ThrottlerModule } from '@nestjs/throttler';
+
+import { APP_GUARD } from '@nestjs/core';
 import { AnimesModule } from './animes/animes.module';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -14,8 +17,20 @@ import { UsersModule } from './users/users.module';
     AnimesModule,
     CharactersModule,
     GameModule,
+    ThrottlerModule.forRoot([
+      {
+        ttl: 60000,
+        limit: 100,
+      },
+    ]),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [
+    AppService,
+    {
+      provide: APP_GUARD,
+      useClass: ThrottlerGuard,
+    },
+  ],
 })
 export class AppModule {}
--- src/game/game.controller.spec.ts
+++ src/game/game.controller.spec.ts
@@ -1,4 +1,5 @@
 import { Test, TestingModule } from '@nestjs/testing';
+
 import { GameController } from './game.controller';
 import { GameService } from './game.service';
 
